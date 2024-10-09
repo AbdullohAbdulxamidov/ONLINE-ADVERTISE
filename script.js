c9e1740f33dbd997381ba308c82100c98b6e2778
ABDULLOH <152079357+AbdullohAbdulxamidov@users.noreply.github.com>
2024-10-09 21:51:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const question1 = document.getElementById('question-1');
+const answer1 = document.getElementById('answer-1');
+const question2 = document.getElementById('question-2');
+const answer2 = document.getElementById('answer-2');
+const question3 = document.getElementById('question-3');
+const answer3 = document.getElementById('answer-3');
+
+
+question1.addEventListener('click', function() {
+    answer1.style.display = 'block';
+})
+
+
+question2.addEventListener('click', function() {
+    answer2.style.display = 'block';
+})
+
+
+question3.addEventListener('click', function() {
+    answer3.style.display = 'block';
+})
